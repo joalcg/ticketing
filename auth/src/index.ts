1467b1d8077e05506a0dcda458e6b89610d0bcbf
Jose Cerdas <jcerdas@extraspace.com>
2021-04-09 20:54:26 -0600

    Setup authentication

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose, { mongo } from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -9,10 +10,18 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+import { cookie } from 'express-validator';
 
 const app = express();
+app.set('trust proxy', true);
 
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+)
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -26,6 +35,10 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT must be defined');
+    }
+
     try {        
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
             useNewUrlParser: true,
