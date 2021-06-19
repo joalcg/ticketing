ef2fb8c1b9856b8c055bec49605448577a875fbc
Jose Cerdas <jcerdas@extraspace.com>
2021-06-18 20:05:51 -0600

    Create payments service

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,99 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { OrderStatus } from '@joalcgtickets/common';
+import { stripe } from '../../stripe';
+import { Payment } from '../../models/payment';
+
+jest.mock('../../stripe');
+
+it('returns a 404 when purchasing an order that does not exist', async () => {
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin())
+        .send({
+            token: 'saddsa',
+            orderId: mongoose.Types.ObjectId().toHexString()
+        })
+        .expect(404);
+});
+
+it('returns a 401 when purchasing an order that does not belong to the user', async () => {
+    const order = Order.build({
+        id: mongoose.Types.ObjectId().toHexString(),
+        userId: mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        price: 20,
+        status: OrderStatus.Created
+    });
+
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin())
+        .send({
+            token: 'saddsa',
+            orderId: order.id
+        })
+        .expect(401);
+});
+
+it('returns a 400 when purchasing a cancelled order', async () => {
+    const userId = mongoose.Types.ObjectId().toHexString();
+
+    const order = Order.build({
+        id: mongoose.Types.ObjectId().toHexString(),
+        userId: userId,
+        version: 0,
+        price: 20,
+        status: OrderStatus.Cancelled
+    });
+
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin(userId))
+        .send({
+            token: 'saddsa',
+            orderId: order.id
+        })
+        .expect(400);
+});
+
+it('returns a 201 with valid inputs', async () => {
+    const userId = mongoose.Types.ObjectId().toHexString();
+
+    const order = Order.build({
+        id: mongoose.Types.ObjectId().toHexString(),
+        userId: userId,
+        version: 0,
+        price: 20,
+        status: OrderStatus.Created
+    });
+
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin(userId))
+        .send({
+            token: 'tok_visa',
+            orderId: order.id
+        })
+        .expect(201);
+
+    const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+
+    expect(chargeOptions.source).toEqual('tok_visa');
+    expect(chargeOptions.amount).toEqual(20 * 100);
+    expect(chargeOptions.currency).toEqual('usd');
+
+    // const payment = await Payment.findOne({
+    //     orderId: order.id,
+    //     stripeId: stripeCharge!.id
+    // });
+    // expected(payment).not.toBeNull();
+});
